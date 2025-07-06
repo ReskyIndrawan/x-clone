2af4e8bb4b51aa24d99093f5037c4ede95f0ea80
Resky Indrawan <indrawanresky.tlg@gmail.com>
2025-07-06 12:28:41 +0900

    Notif and Arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import commentRoute from "./routes/comment.route.js";
+import notificationRoute from "./routes/notification.route.js";
 
 const app = express();
 
@@ -14,12 +16,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware());
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("api/users", userRoute);
 app.use("api/posts", postRoute);
 app.use("api/comments", commentRoute);
+app.use("api/notifications", notificationRoute);
 
 const startServer = async () => {
   try {
